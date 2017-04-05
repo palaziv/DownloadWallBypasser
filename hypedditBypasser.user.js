f5b88bf0707d9f2b6e2b1ff384de48aee259aed0
Ivo Palazzolo <cortex42@users.noreply.github.com>
2017-04-05 20:00:02 +0200

    Update hypedditBypasser.user.js
    Script did not work anymore. Fixed.

--- hypedditBypasser.user.js
+++ hypedditBypasser.user.js
@@ -1,18 +1,8 @@
 // ==UserScript==
 // @name           Hypeddit Bypasser
-// @description    Download a track from Hypeddit without having to comment and follow. Just click the button.
+// @description    Download a track from Hypeddit without having to comment and follow.
 // @include        http://hypeddit.com/track/sc/*
 // @version        1
 // ==/UserScript==
 
-var downloadButton = document.createElement("input");
-downloadButton.type = "button";
-downloadButton.value = "Download directly";
-downloadButton.onclick = downloadTrack;
-downloadButton.setAttribute("style", "position:absolute;top:250px;right:50px;");
-document.body.appendChild(downloadButton);
-
-function downloadTrack(){
-    document.getElementById("comment_text").value = " "; //fill comment box
-    return filedownload();
-}
+filedownload();
